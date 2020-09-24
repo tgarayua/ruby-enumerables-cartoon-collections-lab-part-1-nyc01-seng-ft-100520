77020b178e98156d5a5317c502b61dd9a20cfc9e
Thomas Garayua <tgarayua@gmail.com>
2020-09-24 21:42:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |element|
-    print element.each_with_index
+  array.each_with_index do |element, index|
+    print element[index]
   end
 end
 
